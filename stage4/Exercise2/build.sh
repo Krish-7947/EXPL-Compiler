975032a8252d1294c48f7e6dfc38da12c6263895
Krish <krishdhameliya1886@gmail.com>
2026-09-02 00:10:57 +0530

    Stage 4 Exercise 2 done

---
+++ stage4/Exercise2/build.sh
@@ -0,0 +1,21 @@
+echo "1. Compiling Lex..."
+lex exprtree.l
+
+echo "2. Compiling Yacc..."
+yacc -d exprtree.y
+
+echo "3. Compiling C files..."
+gcc y.tab.c lex.yy.c -o compiler
+
+echo "4. Creating target.xsm..."
+# ./compiler hello.txt
+./compiler test.txt
+
+echo "5. Compiling and running ltranslate to create final machine code..."
+lex ltranslate.l
+gcc lex.yy.c
+./a.out
+
+echo "-----------------------------------------"
+echo "Ready to go...!"
+echo "-----------------------------------------"
